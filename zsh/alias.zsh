2f77277ce66f2015e8ae2671737662174fccdc58
Fabian Hinz <hallo@naibafo.de>
2016-03-09 19:21:46 +0100

    cleanup aliases, grep with nocorrect

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -3,24 +3,15 @@
 # Miscellanneous
 #==============================================================================
 
-alias grep='grep --color=always -d skip'
-alias cp="cp -i"                          # confirm before overwriting something
-alias df='df -h'                          # human-readable sizes
-alias free='free -m'                      # show sizes in MB
+alias vi=vim
+alias view='vim -R'
+alias grep='nocorrect grep --color=always -d skip'
+alias cp='nocorrect cp -i' # confirm before overwriting something
+alias df='df -h' # human-readable sizes
+alias free='free -m' # show sizes in MB
 
 #==============================================================================
 # Nice ls
 #==============================================================================
 alias ls='ls -FX --group-directories-first --color=always'
-
-#==============================================================================
-# Location alias
-#==============================================================================
-alias projects='cd ~/projects'
-
-#==============================================================================
-# Git
-#==============================================================================
-alias glg='git lg'
-alias gst='git st'
 
