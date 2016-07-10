849455fae9d6575b4cbacd1e72b40fbe2e514fe9
Fabian Hinz <hallo@naibafo.de>
2016-07-10 16:09:42 +0200

    modifiy install output

--- vim/install-vim_plug.sh
+++ vim/install-vim_plug.sh
@@ -2,10 +2,10 @@ FILE="$HOME/.vim/autoload/plug.vim"
 
 #!/bin/bash
 if [ ! -f "$FILE" ]; then
-    echo "Installing vim-plug";
+    echo "  Installing vim-plug";
     mkdir -p $(dirname "$FILE")
     curl -fLo "$FILE" --create-dirs "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim";
 else
-    echo "vim-plug already installed"
+    echo "  vim-plug already installed"
 fi
 exit
--- zsh/install-zim.sh
+++ zsh/install-zim.sh
@@ -2,9 +2,9 @@ DIR="$HOME/.zim"
 
 #!/bin/bash
 if [ ! -d "$DIR" ]; then
-    echo "Installing zim";
+    echo "  Installing zim";
     git clone --recursive https://github.com/Eriner/zim.git ${ZDOTDIR:-${HOME}}/.zim
 else
-    echo "zim already installed"
+    echo "  zim already installed"
 fi
 exit
