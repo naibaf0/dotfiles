3c1bf458632965b5df9493dac15256b06e29e643
Fabian Hinz <hallo@naibafo.de>
2016-07-10 15:22:19 +0200

    add install script for zim

---
+++ zsh/install-zim.sh
@@ -0,0 +1,10 @@
+DIR="$HOME/.zim"
+
+#!/bin/bash
+if [ ! -d "$DIR" ]; then
+    echo "Installing zim";
+    git clone --recursive https://github.com/Eriner/zim.git ${ZDOTDIR:-${HOME}}/.zim
+else
+    echo "zim already installed"
+fi
+exit
