4104a0fd87f291492d8e45362340aba0c201def4
Fabian Hinz <hallo@naibafo.de>
2017-07-13 23:54:34 +0200

    add wrapper script for lock

---
+++ scripts/lock.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+killall -SIGUSR1 dunst # pause notification demon
+physlock -l # prevent tty switching
+
+i3lock -n
+
+physlock -L # reenable tty switching
+killall -SIGUSR2 dunst # resume notification demon
+
+notify-send "Welcome back!" -i lock-insecure
