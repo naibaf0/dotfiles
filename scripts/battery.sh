95a1c071ee48d9fbf4357037596f7734074d2e16
Fabian Hinz <hallo@naibafo.de>
2017-07-26 00:02:37 +0200

    improve formatting of battery script

--- scripts/battery.sh
+++ scripts/battery.sh
@@ -8,7 +8,7 @@ percent=`echo $acpi | cut -d, -f2 | sed 's/\%//g'`
 time=`echo $acpi | cut -d, -f3 | cut -d: --fields=1,2`
 
 if [ "$status" == "Discharging" ]; then
-  text='- '"$percent"'% ('"$time"')'
+  text='-'"$percent"'% ('"$time"')'
 elif [ "$status" == "Charging" ]; then
   text=' '"$percent"'% ('"$time"')'
 elif [ "$status" == "Full" ]; then
