ed9b8dee1cc13b4a5900ba1548acfa5db8a6ef56
Fabian Hinz <fabian.hinz@de.clara.net>
2018-02-05 09:33:22 +0100

    i3lock-fancy does not work with dualmonitors yet

--- scripts/lock.sh
+++ scripts/lock.sh
@@ -3,7 +3,7 @@
 killall -SIGUSR1 dunst # pause notification demon
 physlock -l # prevent tty switching
 
-i3lock-fancy -n 
+i3lock -n 
 
 physlock -L # reenable tty switching
 killall -SIGUSR2 dunst # resume notification demon
