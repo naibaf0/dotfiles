2ac9ab1c2918977503dd2df83a31a7bc12d0378b
Fabian Hinz <hallo@naibafo.de>
2018-03-06 18:40:32 +0100

    use trizen to check for aur-updates, right click installs updates in terminal

--- scripts/updates.sh
+++ scripts/updates.sh
@@ -2,8 +2,14 @@
 
 # Display the number of available updates in pacman and the AUR
 
+if [ $BLOCK_BUTTON = "3" ]; then
+  $TERMINAL --name="update_installer" -e "trizen -Syu"
+fi
+
 pac=$(checkupdates | wc -l)
-aur=$(checkupdates-aur | wc -l)
+if ! aur=$(trizen -Su --aur --quiet | wc -l); then
+  aur=0
+fi
 
 updates=$((pac + aur))
 if [ "$updates" -gt 0 ]; then
