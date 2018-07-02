96fc7419e3b72315adc9c4076956e2ff79c87123
Fabian Hinz <fabian.hinz@de.clara.net>
2018-07-02 09:58:28 +0200

    Fix update script and counter

--- scripts/updates.sh
+++ scripts/updates.sh
@@ -5,11 +5,13 @@
 source `dirname $0`/colors.sh
 
 if [ $BLOCK_BUTTON = "3" ]; then
-  $TERMINAL --name="update_installer" -e "trizen -Syu"
+  $TERMINAL --name="update_installer" -e "yay -Syu"
 fi
 
-updates=$(yay -Pn)
+pac=$(checkupdates | wc -l)
+aur=$(yay -Pn)
 
+updates=$pac+$aur
 if [ "$updates" -gt 0 ]; then
   echo " <b>$updates</b>"
   echo " <b>$updates</b>"
