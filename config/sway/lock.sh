8ad7c850cad42e8a51ac0a7c0af9f89e9448083d
Fabian Hinz <hallo@naibafo.de>
2018-10-25 03:48:14 +0200

    changes for migration to sway beta

--- config/sway/lock.sh
+++ config/sway/lock.sh
@@ -1,22 +1,18 @@
 #!/bin/bash
 IMAGE=/tmp/screen_locked.png
 
-xset b off
-
 # take a screenshot and blur the image
-scrot $IMAGE
+grim $IMAGE
 convert -blur 10x10 $IMAGE $IMAGE
 
 killall -SIGUSR1 dunst # pause notification demon
 physlock -l # prevent tty switching
 
-# lock screen with i3lock
+# lock screen with swaylock
 # -n --no-fork
 # -e --no-empty-passwords
 # -f --failed-attempts
-swaylock -n -e -f -i $IMAGE 
+swaylock -n -e -i $IMAGE 
 
 physlock -L # reenable tty switching
 killall -SIGUSR2 dunst # resume notification demon
-
-xset b on
