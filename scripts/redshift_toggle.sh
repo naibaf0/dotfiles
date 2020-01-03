1a09a2f0f167a4d8d5afe9ff74638623f21a4dc7
Fabian Hinz <hallo@naibafo.de>
2020-01-03 19:32:19 +0100

    Update redshift_toggle and use full icon path for notification

--- scripts/redshift_toggle.sh
+++ scripts/redshift_toggle.sh
@@ -4,10 +4,10 @@ RSFILE=${RSFILE:-/tmp/${USER}/rsFile}
 
 if [ -e $RSFILE ]; then
     redshift -c $HOME/.config/redshift/redshift.conf &
-    notify-send "Redshift" "<i>enabled</i>" -i redshift-status-on
+    notify-send "Redshift" "<i>enabled</i>" -i /usr/share/icons/hicolor/scalable/apps/redshift-status-on.svg
     rm $RSFILE
 else
     echo $ "dummytext" >> $RSFILE
     pgrep -f "redshift -c $HOME/.config/redshift/redshift.conf" | xargs kill
-    notify-send "Redshift" "<i>disabled</i>" -i redshift-status-off
+    notify-send "Redshift" "<i>disabled</i>" -i /usr/share/icons/hicolor/scalable/apps/redshift-status-off.svg
 fi
