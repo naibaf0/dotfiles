9c6a6a365dc5ec446b59245a342eff5f2c6e68f9
Fabian Hinz <hallo@naibafo.de>
2020-01-03 19:35:49 +0100

    Add compton icon to picom toggle

--- scripts/picom_toggle.sh
+++ scripts/picom_toggle.sh
@@ -5,11 +5,11 @@ CMPFILE=${CMPFILE:-/tmp/${USER}/cmpFile}
 if [ -e $CMPFILE ]; then
     picom -b &
     sleep 1
-    notify-send "picom" "<i>enabled</i>"
+    notify-send "picom" "<i>enabled</i> -i compton"
     rm $CMPFILE
 else
     echo $ "dummytext" >> $CMPFILE
     pgrep -f "picom -b" | xargs kill
     sleep 1
-    notify-send "picom" "<i>disabled</i>"
+    notify-send "picom" "<i>disabled</i> -i compton"
 fi
