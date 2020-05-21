d9ef71df51501029d4569285227e34b38506c3a2
Fabian Hinz <hallo@naibafo.de>
2020-05-21 19:44:20 +0100

    Work on notification icons

--- scripts/battery-notif.sh
+++ scripts/battery-notif.sh
@@ -14,5 +14,5 @@ PERC=$((($REM * 100) / $FULL))
 
 # If not charging and battery is below percentage limit defined above, then send notification
 #if [ $STAT != "Charging" ] && [ $PERC -le $PERC_LIMIT ]; then
-  /usr/bin/notify-send -u critical -a "system" "Low battery!" "Plug in a charger ASAP!"
+  /usr/bin/notify-send -i battery-empty -u critical -a "system" "Low battery!" "Plug in a charger ASAP!"
 #fi
