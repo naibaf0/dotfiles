c604f620fe43ae25dae9c61f35aae2158c6d1de3
Fabian Hinz <hallo@naibafo.de>
2020-05-21 19:04:05 +0100

    [updates.sh] improve scripting

--- scripts/updates.sh
+++ scripts/updates.sh
@@ -8,10 +8,10 @@ if [ "$BLOCK_BUTTON" = "3" ]; then
   $TERMINAL --name="update_installer" -e "yay"
 fi
 
-pac=$(checkupdates | wc -l)
-aur=$(yay -Pn --aur)
+arch=$(checkupdates 2> /dev/null | wc -l)
+aur=$(yay -Qum 2> /dev/null | wc -l)
+updates=$((arch+aur))
 
-updates=$((pac+aur))
 if [ "$updates" -gt 0 ]; then
   echo " <b>$updates</b>"
   echo " <b>$updates</b>"
