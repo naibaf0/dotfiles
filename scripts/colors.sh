6f9f18337d73b877f5994ae9c3b73b451c24aeb9
Fabian Hinz <hallo@naibafo.de>
2020-05-21 19:03:46 +0100

    [colors.sh] add colornames to colors.sh script for local keeping

--- scripts/colors.sh
+++ scripts/colors.sh
@@ -39,3 +39,27 @@ color18="#3a3c4e"
 color19="#626483"
 color20="#62d6e8"
 color21="#f1f2f8"
+
+#define DRACULA_FG             #F8F8F2
+#define DRACULA_BG             #282A36
+#define DRACULA_BG_DARK        #21222C
+#define DRACULA_SELECTION      #44475A
+#define DRACULA_COMMENT        #6272A4
+#define DRACULA_ORANGE         #FFB86C
+
+#define DRACULA_BLACK          #21222C
+#define DRACULA_BRIGHT_BLACK   #6272A4
+#define DRACULA_RED            #FF5555
+#define DRACULA_BRIGHT_RED     #FF6E6E
+#define DRACULA_GREEN          #50FA7B
+#define DRACULA_BRIGHT_GREEN   #69FF94
+#define DRACULA_YELLOW         #F1FA8C
+#define DRACULA_BRIGHT_YELLOW  #FFFFA5
+#define DRACULA_BLUE           #BD93F9
+#define DRACULA_BRIGHT_BLUE    #D6ACFF
+#define DRACULA_MAGENTA        #FF79C6
+#define DRACULA_BRIGHT_MAGENTA #FF92DF
+#define DRACULA_CYAN           #8BE9FD
+#define DRACULA_BRIGHT_CYAN    #A4FFFF
+#define DRACULA_WHITE          #F8F8F2
+#define DRACULA_BRIGHT_WHITE   #FFFFFF
