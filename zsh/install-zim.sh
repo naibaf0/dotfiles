93c17421d980d30692f91544aa1a454598da2838
Fabian Hinz <hallo@naibafo.de>
2020-11-09 22:11:31 +0100

    Update zsh install script

--- zsh/install-zim.sh
+++ zsh/install-zim.sh
@@ -3,7 +3,7 @@ DIR="$HOME/.zim"
 #!/bin/bash
 if [ ! -d "$DIR" ]; then
     echo "  Installing zim";
-    curl -s --proto -all,+https https://raw.githubusercontent.com/zimfw/install/develop/install.zsh | zsh
+    curl -fsSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh | zsh
 else
     echo "  Skipping zim: already installed"
 fi
