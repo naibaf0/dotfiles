beb9ea104ab138b969346e11e0a1f2a45de16f77
Fabian Hinz <hallo@naibafo.de>
2020-11-14 00:15:03 +0100

    Add new fancy swaylock

--- config/sway/lock.sh
+++ config/sway/lock.sh
@@ -1,20 +1,6 @@
 #!/bin/bash
 
-images=()
-swaylock_args=()
-
-swaylock_args+=(-e)
-
-for output in $(swaymsg -t get_outputs | jq -c -r '.[] | select(.active == true) | .name'); do
-  image=$(mktemp --suffix=.png)
-  images+=("$image")
-  swaylock_args+=(-i "$output":"$image")
-  grim -o "$output" "$image"
-done
-
-printf '%s\n' "${images[@]}" | xargs -P 0 -I{} convert -blur 0x8 {} {}
-
 physlock -l #prevent tty switching
-swaylock "${swaylock_args[@]}" -s fit
+swaylock -e --screenshots --clock --indicator --effect-blur 5x5
 physlock -L # reenable tty switching
 rm "${images[@]}"
