d0547fdb3ce3549bac858a673751fc95cb6b7e68
Fabian Hinz <hallo@naibafo.de>
2021-09-15 22:56:51 +0200

    keep i3/xorg branch

--- config/sway/lock.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-physlock -l #prevent tty switching
-swaylock -e --screenshots --clock --indicator --effect-blur 5x5
-physlock -L # reenable tty switching
--- config/waybar/modules/mediaplayer.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-# Placeholder for now. let's create a script, which will choose between spotify and mpd, depending on whats playing
