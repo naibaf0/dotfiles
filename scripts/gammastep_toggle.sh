b2c2cdc0d436ee54f26cfdc574d065d6640a0010
Fabian Hinz <hallo@naibafo.de>
2022-07-17 02:07:30 +0200

    actually scwitch to gammastep

--- scripts/gammastep_toggle.sh
+++ scripts/gammastep_toggle.sh
@@ -3,11 +3,11 @@
 RSFILE=${RSFILE:-/tmp/${USER}/rsFile}
 
 if [ -e $RSFILE ]; then
-    redshift -c $HOME/.config/redshift/redshift.conf &
-    notify-send "Redshift" "<i>enabled</i>" -i /usr/share/icons/hicolor/scalable/apps/redshift-status-on.svg
+    gammastep -c $HOME/.config/gammastep/config.ini &
+    notify-send "Gammastep" "<i>enabled</i>" -i /usr/share/icons/hicolor/scalable/apps/redshift-status-on.svg
     rm $RSFILE
 else
     echo $ "dummytext" >> $RSFILE
-    pgrep -f "redshift -c $HOME/.config/redshift/redshift.conf" | xargs kill
-    notify-send "Redshift" "<i>disabled</i>" -i /usr/share/icons/hicolor/scalable/apps/redshift-status-off.svg
+    pgrep -f "gammastep -c $HOME/.config/gammastep/config.ini" | xargs kill
+    notify-send "Gammastep" "<i>disabled</i>" -i /usr/share/icons/hicolor/scalable/apps/redshift-status-off.svg
 fi
