d1f705bc62d7a8d5e1dc62d083ba2f60dfefb37d
Fabian Hinz <hallo@naibafo.de>
2022-12-11 04:17:36 +0100

    use pwctl and remove pamixer dependency, smarter volume notifications

--- scripts/volume.sh
+++ scripts/volume.sh
@@ -2,25 +2,28 @@
 
 # Configure Volume with notification
 
-if [ "$1" = "up" ]; then
- 	pamixer -i 5
-  notify-send --hint=int:transient:1 --hint=int:value:${$(pamixer --get-volume)%.*} --hint=string:synchronous:volume -i audio-volume-high "Volume up 5 %" ""
-elif [ "$1" = "down" ]; then
-  pamixer -d 5
-  notify-send --hint=int:transient:1 --hint=int:value:${$(pamixer --get-volume)%.*} --hint=string:synchronous:volume -i audio-volume-low "Volume down 5 %" ""
-elif [ "$1" = "mute"  ]; then
- 	  pamixer -t
-  if [ $(pamixer --get-mute) = true ]; then
-    notify-send --hint=string:synchronous:volume -i audio-volume-muted "Muted" ""
-  else
-    notify-send --hint=string:synchronous:volume -i audio-volume-medium "Unmuted" ""
-	fi
-elif [ "$1" = "mic-mute"  ]; then
- 	  pamixer -t --default-source
-  if [ $(pamixer --default-source --get-mute) = true ]; then
-    notify-send --hint=string:synchronous:volume -i mic-off "Muted" ""
-  else
-    notify-send --hint=string:synchronous:volume -i mic-on "Unmuted" ""
-	fi
+if [ "$1" = "vol" ]; then
+    if [ "$2" = "up" ]; then
+        wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+
+    elif [ "$2" = "down" ]; then
+      wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-
+    elif [ "$2" = "mute"  ]; then
+        wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle
+    fi
+
+    VOL=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk '{print $2*100}')
+    print $VOL
+    if [ "$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk '{print $3}')" = "[MUTED]" ]; then
+        notify-send --hint=string:synchronous:volume -i audio-volume-muted "Muted" ""
+    elif [[ $VOL < 34 ]]; then
+        notify-send --hint=int:transient:1 --hint=int:value:$VOL --hint=string:synchronous:volume -i audio-volume-low "${VOL}%" ""
+    elif [[ $VOL < 67 ]]; then
+        notify-send --hint=int:transient:1 --hint=int:value:$VOL --hint=string:synchronous:volume -i audio-volume-medium "${VOL}%" ""
+    else
+        notify-send --hint=int:transient:1 --hint=int:value:$VOL --hint=string:synchronous:volume -i audio-volume-high "${VOL}%" ""
+    fi
+
+elif [ "$1" = "mic" ]; then
+        wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle
 fi
 
