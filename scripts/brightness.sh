f227c1629b985a3329200db7e9e88bc846d80169
Fabian Hinz <hallo@naibafo.de>
2022-12-14 18:12:48 +0100

    group brighthness notifications

--- scripts/brightness.sh
+++ scripts/brightness.sh
@@ -4,10 +4,10 @@
 
 if [ "$1" = "up" ]; then
  	light -A 10
-  notify-send --hint=int:transient:1 --hint=int:value:${$(light -G)%.*} --hint=string:synchronous:brightness -i high-brightness "Brightness up 10 %" ""
+  notify-send --category=brightness --hint=int:transient:1 --hint=int:value:${$(light -G)%.*} --hint=string:synchronous:brightness -i high-brightness "Brightness up 10 %" ""
 else
 	if [ "$1" = "down" ]; then
  	  light -U 10
-    notify-send --hint=int:transient:1 --hint=int:value:${$(light -G)%.*} --hint=string:synchronous:brightness -i low-brightness "Brightness down 10 %" ""
+    notify-send --category=brightness --hint=int:transient:1 --hint=int:value:${$(light -G)%.*} --hint=string:synchronous:brightness -i low-brightness "Brightness down 10 %" ""
 	fi
 fi
