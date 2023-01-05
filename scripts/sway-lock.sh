4dc509079c000b2fe2adc2a6260733d0ce63e068
Fabian Hinz <hallo@naibafo.de>
2023-01-05 23:30:13 +0100

    add graceperiod to swaylock

--- scripts/sway-lock.sh
+++ scripts/sway-lock.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 physlock -l #prevent tty switching
-swaylock -e --screenshots --clock --indicator --effect-blur 5x5
+swaylock -e --grace 30 --screenshots --clock --indicator --effect-blur 5x5
 physlock -L # reenable tty switching
