ebaf8a34f5362eea7c466627be488123d7427e6e
Fabian Hinz <hallo@naibafo.de>
2023-03-10 01:35:43 +0100

    Add script for a power menu with wofi

---
+++ scripts/power-menu.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+MENU="  Logout
+鈴 Suspend
+  Reboot
+  Shutdown"
+
+CHOICE=$(echo "$MENU" | wofi -i --dmenu -L 5 )
+
+case $CHOICE in
+  "  Logout")
+    swaymsg exit;;
+  "鈴 Suspend")
+    exec systemctl suspend;;
+  "  Reboot")
+    exec systemctl reboot;;
+  "  Shutdown")
+    exec systemctl poweroff -i;;
+esac
