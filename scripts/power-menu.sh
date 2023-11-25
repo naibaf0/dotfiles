7e9b38824a350ff67f3d367b7cd83db0ccae26ec
Fabian Hinz <hallo@naibafo.de>
2023-11-25 23:58:54 +0100

    bump

--- scripts/power-menu.sh
+++ scripts/power-menu.sh
@@ -9,11 +9,11 @@ CHOICE=$(echo "$MENU" | wofi -i --dmenu -L 5 -W 15%)
 
 case $CHOICE in
   "󰍃 Logout")
-    swaymsg exit;;
-  "󰒲  Suspend")
-    exec systemctl suspend;;
-  "  Reboot")
-    exec systemctl reboot;;
+    hyperctl dispatch exit;;
+  "󰒲 Suspend")
+    systemctl suspend;;
+  " Reboot")
+    systemctl reboot;;
   "󰐥 Shutdown")
-    exec systemctl poweroff -i;;
+    systemctl poweroff -i;;
 esac
