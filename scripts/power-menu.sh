7d44c0104218d0c33e7735a99f262c5ed709b1c6
Fabian Hinz <hallo@naibafo.de>
2023-12-01 00:33:26 +0100

    Update power-menu and adjust wofi accordingly

--- scripts/power-menu.sh
+++ scripts/power-menu.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
-MENU="󰍃 Logout
-󰒲 Suspend
-󰜉 Reboot
-󰐥 Shutdown"
+MENU="img:/usr/share/icons/breeze-dark/actions/32/system-log-out.svg:text:Logout\nimg:/usr/share/icons/breeze-dark/actions/32/system-suspend.svg:text:Suspend\nimg:/usr/share/icons/breeze-dark/actions/32/system-reboot.svg:text:Reboot\nimg:/usr/share/icons/breeze-dark/actions/32/system-shutdown.svg:text:Shutdown:"
 
-CHOICE=$(echo "$MENU" | wofi -i --dmenu -L 5 -W 15%)
+CHOICE=$(echo -e "$MENU" | wofi --dmenu -L 5 -W 15% --cache-file /dev/null | awk '{print tolower($1)}')
 
 case $CHOICE in
-  "󰍃 Logout")
-    hyperctl dispatch exit;;
-  "󰒲 Suspend")
+  "logout")
+    hyperctl dispatch exit 0;;
+  "suspend")
     systemctl suspend;;
-  " Reboot")
+  "reboot")
     systemctl reboot;;
-  "󰐥 Shutdown")
+  "shutdown")
     systemctl poweroff -i;;
 esac
