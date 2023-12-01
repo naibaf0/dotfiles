4a839c5501c834489b27c7a816ba2204391f7c93
Fabian Hinz <hallo@naibafo.de>
2023-12-01 02:52:51 +0100

    Update battery xcript

--- scripts/battery-notif.sh
+++ scripts/battery-notif.sh
@@ -1,18 +1,22 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-MESSAGE="Low battery."
 PERC_LIMIT=10
 
-BATTERY="BAT1"
-PATH="/sys/class/power_supply/$BATTERY"
+BATTERY="BATT"
+BATTERY_PATH="/sys/class/power_supply/$BATTERY"
 
-FULL=$(/usr/bin/cat $PATH/charge_full)
-REM=$(/usr/bin/cat $PATH/charge_now)
-STAT=$(/usr/bin/cat $PATH/status)
+while true; do
+    FULL=$(/usr/bin/cat $BATTERY_PATH/charge_full)
+    REM=$(/usr/bin/cat $BATTERY_PATH/charge_now)
+    STAT=$(/usr/bin/cat $BATTERY_PATH/status)
 
-PERC=$((($REM * 100) / $FULL))
+    PERC=$((($REM * 100) / $FULL))
 
-# If not charging and battery is below percentage limit defined above, then send notification
-#if [ $STAT != "Charging" ] && [ $PERC -le $PERC_LIMIT ]; then
-  /usr/bin/notify-send -i battery-empty -u critical -a "system" "Low battery!" "Plug in a charger ASAP!"
-#fi
+    # If not charging and battery is below percentage limit defined above, then send notification
+    if [ $STAT = "Discharging" ] && [ $PERC -le $PERC_LIMIT ]; then
+        notify-send -i battery-empty -u critical -a "system" "Low battery!" "Plug in a charger ASAP!"
+        sleep 1200
+    else
+        sleep 120
+    fi
+done
